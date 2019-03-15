df5580f2a7cef644907d56fc3804401cbfc1a710
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2019-03-15 19:32:10 -0300

    commit inicial
    commit 1

---
+++ src/atividade/Conta.java
@@ -0,0 +1,37 @@
+package atividade;
+
+public class Conta {
+	
+	int numero;
+    String titular;
+    double saldo;
+    
+ 	
+	public Conta() {
+		super();
+	}
+
+
+
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+    
+    
+
+}
---
+++ src/atividade/Main.java
@@ -0,0 +1,15 @@
+package atividade;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		
+		conta.setTitular("Leandro");
+		
+		System.out.println("Titular: " + conta.getTitular());
+		
+	
+	}
+
+}
